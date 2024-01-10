008859a0ac58f7f6cbc01935ff482b27c8401cd1
newbe01 <pkwaa3@gmail.com>
2024-01-10 16:37:10 +0900

    spring advanced - ThreadLocal
    쓰레드로컬을 사용해 코드변경, 테스트작성 및 apk적용

--- src/main/java/spring/advanced/LogTraceConfig.java
+++ src/main/java/spring/advanced/LogTraceConfig.java
@@ -4,13 +4,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import spring.advanced.trace.logtrace.FieldLogTrace;
 import spring.advanced.trace.logtrace.LogTrace;
+import spring.advanced.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 
 }
---
+++ src/test/java/spring/advanced/trace/logtrace/ThreadLocalLogTraceTest.java
@@ -0,0 +1,30 @@
+package spring.advanced.trace.logtrace;
+
+import org.junit.jupiter.api.Test;
+import spring.advanced.trace.TraceStatus;
+
+class ThreadLocalLogTraceTest {
+
+    ThreadLocalLogTrace trace = new ThreadLocalLogTrace();
+
+    @Test
+    void begin_end_level2() {
+
+        TraceStatus status1 = trace.begin("1");
+        TraceStatus status2 = trace.begin("2");
+
+        trace.end(status2);
+        trace.end(status1);
+    }
+
+    @Test
+    void begin_exception_level2() {
+
+        TraceStatus status1 = trace.begin("1");
+        TraceStatus status2 = trace.begin("2");
+
+        trace.exception(status2, new IllegalStateException());
+        trace.exception(status1, new IllegalStateException());
+    }
+
+}
