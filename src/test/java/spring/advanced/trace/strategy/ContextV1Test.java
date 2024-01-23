121a2f28bc5f83369188df49c411449eda2c78c6
newbe01 <pkwaa3@gmail.com>
2024-01-23 16:06:22 +0900

    spring advanced - strategy ex2

--- src/test/java/spring/advanced/trace/strategy/ContextV1Test.java
+++ src/test/java/spring/advanced/trace/strategy/ContextV1Test.java
@@ -3,6 +3,7 @@ package spring.advanced.trace.strategy;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import spring.advanced.trace.strategy.code.strategy.ContextV1;
+import spring.advanced.trace.strategy.code.strategy.Strategy;
 import spring.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import spring.advanced.trace.strategy.code.strategy.StrategyLogic2;
 
@@ -45,5 +46,63 @@ public class ContextV1Test {
         ContextV1 context2 = new ContextV1(logic2);
         context2.execute();
 
+    }
+
+    @Test
+    void strategyV2() {
+        Strategy strategy1 = new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("logic 1");
+            }
+        };
+
+        ContextV1 contextV1 = new ContextV1(strategy1);
+        contextV1.execute();
+
+        Strategy strategy2 = new Strategy() {
+
+            @Override
+            public void call() {
+                log.info("logic 2");
+            }
+        };
+
+        ContextV1 contextV2 = new ContextV1(strategy2);
+        contextV2.execute();
+
+        log.info("strategy1 : {}", strategy1.getClass());
+        log.info("strategy2 : {}", strategy2.getClass());
+
+    }
+
+    @Test
+    void strategyV3() {
+        ContextV1 contextV1 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("logic 1");
+            }
+        });
+        contextV1.execute();
+
+        ContextV1 contextV2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("logic 2");
+            }
+        });
+
+        contextV2.execute();
+    }
+
+    @Test
+    void strategyV4() {
+        ContextV1 contextV1 = new ContextV1(() -> log.info("logic 1"));
+        contextV1.execute();
+
+        ContextV1 contextV2 = new ContextV1(() -> log.info("logic 2"));
+        contextV2.execute();
     }
 }
