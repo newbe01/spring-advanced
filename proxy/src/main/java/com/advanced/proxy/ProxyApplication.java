31ddf5d8000d449788a707cfeea15d3b197d70b6
newbe01 <pkwaa3@gmail.com>
2024-02-19 20:07:34 +0900

    proxyfactory V1, V2

--- proxy/src/main/java/com/advanced/proxy/ProxyApplication.java
+++ proxy/src/main/java/com/advanced/proxy/ProxyApplication.java
@@ -2,6 +2,8 @@ package com.advanced.proxy;
 
 import com.advanced.proxy.config.v2_dynamicproxy.DynamicProxyBasicConfig;
 import com.advanced.proxy.config.v2_dynamicproxy.DynamicProxyFilterConfig;
+import com.advanced.proxy.config.v3_proxyfactory.ProxyFactoryConfigV1;
+import com.advanced.proxy.config.v3_proxyfactory.ProxyFactoryConfigV2;
 import com.advanced.proxy.trace.logtrace.LogTrace;
 import com.advanced.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -14,7 +16,9 @@ import org.springframework.context.annotation.Import;
 //@Import(InterfaceProxyConfig.class)
 //@Import(ConcreteProxyConfig.class)
 //@Import(DynamicProxyBasicConfig.class)
-@Import(DynamicProxyFilterConfig.class)
+//@Import(DynamicProxyFilterConfig.class)
+//@Import(ProxyFactoryConfigV1.class)
+@Import(ProxyFactoryConfigV2.class)
 @SpringBootApplication(scanBasePackages = "com.advanced.proxy.app.v3")
 public class ProxyApplication {
 
